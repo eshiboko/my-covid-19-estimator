fece9a0ee1a5fdb6f6e4d2d915da31c06c8afd61
eshiboko <62471370+eshiboko@users.noreply.github.com>
2020-04-10 12:50:03 +0300

    Update estimator.js
    build challenges

--- src/estimator.js
+++ src/estimator.js
@@ -1,3 +1,10 @@
-const covid19ImpactEstimator = (data) => data;
+const covid19ImpactEstimator = (data) 
+	const input = data;
+	return {
+		data: input,
+		impact: {}
+		severeImpact: {}
+	}:
+}:
 
-export default covid19ImpactEstimator;
+export default covid19ImpactEstimator
