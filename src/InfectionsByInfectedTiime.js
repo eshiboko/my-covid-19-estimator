4e705b595e0d7b37217b8cc8cafc56e19388a5b1
eshiboko <62471370+eshiboko@users.noreply.github.com>
2020-04-10 15:53:09 +0300

    start building estimator

---
+++ src/InfectionsByInfectedTiime.js
@@ -0,0 +1,17 @@
+const covid19ImpactEstimator = (data) => data;
+const covid19ImpactEstimator = () 
+	const input = data;
+	return {
+		data: input,
+		impact: {ReportedCases:(*10)
+		}
+		severeImpact:
+			{CurrentlyInfected:(50*)
+				CurrentlyInfecte*(2 to the power of 9)
+				CurrentlyInfected *450
+		}
+	}:
+}:
+
+export default covid19ImpactEstimator;
+export default covid19ImpactEstimator
