b5d002c14e232c841d29758d7d218b23691445a1
eshiboko <62471370+eshiboko@users.noreply.github.com>
2020-04-11 14:54:26 +0300

    Update InfectionsByRequestedTiime.js

--- src/InfectionsByRequestedTiime.js
+++ src/InfectionsByRequestedTiime.js
@@ -3,22 +3,39 @@ const covid19ImpactEstimator = ()
 	const input = data;
 	return {
 		data: input,
+	data: {
+		region:{
+			"name": "Kenya"
+			"avgAge": 20
+			"avgDailyIncomeInUSD": 2
+			"avgDailyIncomePopulation": 193
+		}
 	}
+	}
+		"periodType": "days"
+		"timeToElapse": 28
+		"reportedCases": *10
+		"population": 49007000
 		"estimate":{
 			"impact": {
 				"currentlyInfected": 8950
 				"infectionsByRequestedTime": 80550
 				"SevereCasesByrequestedTime": 40275
+				"hospitalBedsByRequestedTime":- 500
+				"casesForIcuByRequestedTime": 3340347
+				"casesForVentilatorsByRequestedTime":2264432
+				"dollarsInFlight":
 		}
 		"severeImpact": {
 			"currentlyInfected": 724950
 			"infectionsByRequestedTime": 6524550
-			"SevereCasesByrequestedTime": 5799600
+			"SevereCasesByrequestedTime": 978682
+			"hospitalBedsByRequestedTime": 5545868
+			"casesForIcuByRequestedTime": 36247
+			"casesForVentilatorsByRequestedTime": 130491
+			"dollarsInFlight": 1592727
 				
 		}
-		}
 	
-
-
 export default covid19ImpactEstimator;
 export default covid19ImpactEstimator
